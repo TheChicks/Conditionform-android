320e91865df9236eacb6387353190408d7e2445d
blazingnky <blazingnky@gmail.com>
2016-04-15 21:20:48 +0900

    #14 pill details view complete

---
+++ app/src/main/java/com/thechicks/conditionform/PillDetailsActivity.java
@@ -0,0 +1,82 @@
+package com.thechicks.conditionform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class PillDetailsActivity extends AppCompatActivity {
+    @Bind(R.id.IvPill)
+    ImageView ivPill;
+
+    @Bind(R.id.tvPillName)
+    TextView tvPillName;
+
+    @Bind(R.id.tvCategoryWelfare)
+    TextView tvCategoryWelfare;
+
+    @Bind(R.id.tvManufactureAssort)
+    TextView tvManufactureAssort;
+
+    @Bind(R.id.tvAssortment)
+    TextView tvAssortment;
+
+    @Bind(R.id.tvInsuranceCode)
+    TextView tvInsuranceCode;
+
+    @Bind(R.id.tvPregnantRating)
+    TextView tvPregnantRating;
+
+    @Bind(R.id.tvAgeProhibit)
+    TextView tvAgeProhibit;
+
+    @Bind(R.id.tvShapeInfoAppearance)
+    TextView tvShapeInfoAppearance;
+
+    @Bind(R.id.tvShapeInfoFormulation)
+    TextView tvShapeInfoFormulation;
+
+    @Bind(R.id.tvShapeInfoShape)
+    TextView tvShapeInfoShape;
+
+    @Bind(R.id.tvShapeInfoColor)
+    TextView tvShapeInfoColor;
+
+    @Bind(R.id.tvShapeInfoIdMark)
+    TextView tvShapeInfoIdMark;
+
+    @Bind(R.id.tvIngredientInfo)
+    TextView tvIngredientInfo;
+
+    @Bind(R.id.tvStoragintMethod)
+    TextView tvStoragintMethod;
+
+    @Bind(R.id.tvEfficacy)
+    TextView tvEfficacy;
+
+    @Bind(R.id.tvDosage)
+    TextView tvDosage;
+
+    @Bind(R.id.tvPrecaution)
+    TextView tvPrecaution;
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill_details);
+
+        ButterKnife.bind(this);
+
+        mToolbar.setTitle("알람등록");
+        setSupportActionBar(mToolbar);
+
+    }
+
+}
