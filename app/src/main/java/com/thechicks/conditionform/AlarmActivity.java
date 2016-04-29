3699559cf502c6e15703a83559e35b43b1503fbd
DongHee Kim <opklnm102@gmail.com>
2016-04-29 17:48:45 +0900

    #12 AlarmKlaxon 구현

--- app/src/main/java/com/thechicks/conditionform/AlarmActivity.java
+++ app/src/main/java/com/thechicks/conditionform/AlarmActivity.java
@@ -9,10 +9,11 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class AlarmActivity extends AppCompatActivity {
+
     AlarmListAdapter mAlarmListAdapter;
 
     @Bind(R.id.recyclerview_alarm_item)
-    RecyclerView recyclerview_alarm_item;
+    RecyclerView rvAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,24 @@ public class AlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm);
         ButterKnife.bind(this);
 
+        rvAlarm.setLayoutManager(new LinearLayoutManager(AlarmActivity.this, LinearLayoutManager.VERTICAL, false));
+        rvAlarm.setHasFixedSize(true);
 
-        recyclerview_alarm_item.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
-        recyclerview_alarm_item.setHasFixedSize(true);
+        mAlarmListAdapter = new AlarmListAdapter(AlarmActivity.this);
+        rvAlarm.setAdapter(mAlarmListAdapter);
+    }
 
-        mAlarmListAdapter = new AlarmListAdapter(getApplicationContext());
-        recyclerview_alarm_item.setAdapter(mAlarmListAdapter);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AlarmKlaxon.start(AlarmActivity.this, null, false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        AlarmKlaxon.stop(AlarmActivity.this);
     }
 }
---
+++ app/src/main/java/com/thechicks/conditionform/AlarmInstance.java
@@ -0,0 +1,24 @@
+package com.thechicks.conditionform;
+
+import android.net.Uri;
+
+/**
+ * Created by Administrator on 2016-04-28.
+ */
+public class AlarmInstance {
+
+    long mId;
+    int mYear;
+    int mMonth;
+    int mDay;
+    int mHour;
+    int mMinute;
+    String mLabel;
+    boolean mVibrate;
+    Uri mRingtone;
+    long mAlarmId;
+    int mAlarmState;
+
+
+
+}
