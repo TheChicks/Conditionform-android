8e5daf705b7b0b11f58517c9c9d4df41b4962759
DongHee Kim <opklnm102@gmail.com>
2016-05-13 15:23:23 +0900

    #7 api serer ip 추가

--- app/src/main/java/com/thechicks/conditionform/util/Constants.java
+++ app/src/main/java/com/thechicks/conditionform/util/Constants.java
@@ -1,15 +1,17 @@
 package com.thechicks.conditionform.util;
 
+import com.thechicks.conditionform.BuildConfig;
+
 /**
  * Created by opklnm102 on 2016-04-29.
  */
 public class Constants {
 
     // geneymotion localhost
-    public static final String ENDPOINT = "http://10.0.3.2:8080/";
+//    public static final String ENDPOINT = BuildConfig.LOCAL_HOST_IP;
 
     // api server
-//    public static final String ENDPOINT = "http://127.0.0.1:8080/";
+    public static final String ENDPOINT = BuildConfig.API_SERVER_IP;
 
     public static final String PREF_NAME = "pref_settings";
     public static final String PREF_VIBRATE = "pref_vibrate";
